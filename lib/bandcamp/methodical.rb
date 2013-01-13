0ce557c661e87c6f6b523d26517653855a57e3d2
Mike Williamson <blessedbyvirtuosity@gmail.com>
2013-01-12 22:34:45 -0500

    Moved mixin into the BandCamp namespace.

--- lib/bandcamp/methodical.rb
+++ lib/bandcamp/methodical.rb
@@ -1,13 +1,15 @@
-module Methodical
+module BandCamp
+  module Methodical
 
-  def to_methods hash
-    eigenclass = class << self; self; end
-    hash.each_pair do |key, val|
-       eigenclass.instance_eval { attr_reader key.to_sym }
-       instance_variable_set("@#{key}".to_sym, val)
+    def to_methods hash
+      eigenclass = class << self; self; end
+      hash.each_pair do |key, val|
+         eigenclass.instance_eval { attr_reader key.to_sym }
+         instance_variable_set("@#{key}".to_sym, val)
+      end
     end
+
+    private :to_methods
+
   end
-
-  private :to_methods
-
 end

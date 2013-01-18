faa844a74bc42df41539a75f78b8ccb5f0055488
Mike Williamson <blessedbyvirtuosity@gmail.com>
2013-01-17 21:58:39 -0500

    Cleaned up Configuration and added specs

--- lib/bandcamp/configuration.rb
+++ lib/bandcamp/configuration.rb
@@ -5,11 +5,6 @@ module BandCamp
 
     attr_writer :api_key
 
-    def initialize
-      @api_key ||= ""
-      @uri = "http://api.bandcamp.com"
-    end
-
     def api_key
       raise ConfigurationError, "You need to set an API key" unless @api_key
       @api_key
---
+++ spec/configuration_spec.rb
@@ -0,0 +1,23 @@
+require 'bandcamp/configuration'
+
+module BandCamp
+  describe Configuration do
+
+    describe "#api_key" do
+
+      let(:config){ Configuration.new }
+
+      it "returns the key" do
+        config.api_key = "abc123"
+        expect(config.api_key).to eq "abc123"
+      end
+
+      context "when there is no key" do
+        it "raises an error" do
+          expect{ config.api_key }.to raise_error ConfigurationError
+        end
+      end
+
+    end
+  end
+end

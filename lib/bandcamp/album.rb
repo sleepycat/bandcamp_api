2162becb10f382b72d70ad3599ea3b8dc51aab19
Mike Williamson <blessedbyvirtuosity@gmail.com>
2013-01-26 11:20:43 -0500

    Album now includes Associated.
    Plus a little refactoring of the album spec.

--- lib/bandcamp/album.rb
+++ lib/bandcamp/album.rb
@@ -1,9 +1,11 @@
+require 'bandcamp/associated'
 require 'bandcamp/methodical'
 
 module Bandcamp
   class Album
 
     include Methodical
+    include Associated
 
     def initialize album_hash
       to_methods album_hash
--- spec/album_spec.rb
+++ spec/album_spec.rb
@@ -2,10 +2,21 @@ require 'bandcamp/album'
 
 module Bandcamp
   describe Album do
+
+    let(:album){ Album.new(foo: "bar", band_id: 123456) }
+
+    it "includes the Associated Module" do
+      expect(album.private_methods).to include :retrieve_associated
+    end
+
+    it "includes the Methodical module" do
+      expect(album.private_methods).to include :to_methods
+    end
+
     describe ".new" do
 
       it "accepts a hash and returns an Album" do
-        expect(Album.new(foo: "bar")).to be_an Album
+        expect(album).to be_an Album
       end
 
       it "creates methods based on the hash" do
